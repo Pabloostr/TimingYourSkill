461f68be2b01ac7bc1591646d93ad6fcaae6258f
Pablo <ikolochko22@gmail.com>
2021-03-31 21:26:43 +0300

    Done tasks dont work

--- TimingYourSkill3.0/controller/DoneTasksTableViewController.swift
+++ TimingYourSkill3.0/controller/DoneTasksTableViewController.swift
@@ -9,10 +9,43 @@ import UIKit
 
 class DoneTasksTableViewController: UITableViewController {
     
+    private var tasks: [Task] = [] {
+        didSet{
+            tableView.reloadData()
+        }
+    }
+    private let databaseManager = DatabaseManeger()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.backgroundColor = .red
+        addTasksListener()
+    }
+    
+    private func addTasksListener() { /// Зберігає данні введені в tableview
+        databaseManager.addTasksListener(forDoneTasks: true) { [weak self] (result) in
+            switch result{
+            case .success(let tasks):
+                self?.tasks = tasks
+            case .failure(let error):
+                print(error )
+            }
+        }
+    }
+    
+}
+
+
+extension DoneTasksTableViewController {
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellId", for: indexPath) as! DoneTaskTableViewCell
+        let task = tasks[indexPath.item]
+        cell.configure(with: task)
+        return cell
     }
     
 }
---
+++ TimingYourSkill3.0/view/DoneTaskTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  DoneTaskTableViewCell.swift
+//  TimingYourSkill3.0
+//
+//  Created by Pavlo Ostrozhynskyi on 31.03.2021.
+//
+
+import UIKit
+
+class DoneTaskTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    func configure(with task: Task) {
+        titleLabel.text = task.title
+    }
+    
+}
